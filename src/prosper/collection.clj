7116062c7d6bb9a15d330836d12acb0d178eff3a
Wyatt Alt <wyaltster@gmail.com>
2015-11-22 16:24:12 -0800

    experimental logging changes

--- src/prosper/collection.clj
+++ src/prosper/collection.clj
@@ -33,28 +33,47 @@
       (Thread/sleep (if (in-release?) *release-rate* *base-rate*))
       (as/>!! future-ch (query/kit-get "Listings")))))
 
+(defn log-delta
+  [delta]
+  (let [listingnumber (first delta)
+        {:keys [ProsperRating LenderYield AmountRemaining]} (second delta)]
+    (log/infof "updating market state: %s: (%s / %s / %s), " listingnumber ProsperRating LenderYield AmountRemaining)))
+
+(defn log-deltas
+  [deltas]
+  (doseq [d deltas]
+    (log-delta d)))
+
+(defn weak-
+  [a b]
+  (try
+    (- a b)
+    (catch Exception _
+      a)))
+
+(defn coalesce-state
+  [acc listing]
+  (assoc acc (:ListingNumber listing)
+    (select-keys listing [:AmountRemaining :LenderYield :ProsperRating])))
+
+(defn listings->market-state
+  [listings]
+  (->> listings
+       (reduce coalesce-state {})))
+
 (defn value-diffs
   "extract only listings for which amountremaining has decreased"
   [old new-state]
   (let [new-values (->> new-state
                         ;; next line is dumb
-                        (filter #(< (second %) (or (get old (first %)) 100000)))
+                        (filter #(< (:AmountRemaining (second %)) (or (:AmountRemaining (get old (first %))) 100000)))
                         (into {}))
-        deltas (merge-with - new-values (select-keys old (keys new-values)))]
+        deltas (merge-with (partial merge-with weak-) new-values (select-keys old (keys new-values)))]
     [new-values deltas]))
-
-(defn log-deltas
-  [deltas]
-  (->> deltas
-       (map #(format "%s: %s, " (first %) (second %)))
-       (string/join #" ")
-       (log/infof "updating market state: %s")))
 
 (defn update-state
   [new-listings]
-  (let [s' (->> new-listings
-                (map #(select-keys % [:ListingNumber :AmountRemaining]))
-                (reduce #(assoc %1 (:ListingNumber %2) (:AmountRemaining %2)) {}))]
+  (let [s' (listings->market-state new-listings)]
     (swap! market-state
            (fn [s]
              (let [[diffs deltas] (value-diffs s s')]
