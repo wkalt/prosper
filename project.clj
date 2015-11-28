701fc378b74855673134d90653e99e35605618dd
Wyatt Alt <wyaltster@gmail.com>
2015-11-27 23:32:27 -0800

    update at-at

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [environ "1.0.1"]
-                 [overtone/at-at "1.2.0"]
+                 [silasdavis/at-at "1.2.0"]
                  [compojure "1.2.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/java.jdbc "0.3.6"]
