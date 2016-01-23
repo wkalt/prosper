6554385cc550b0c8228721c83ad274dd26e09837
Wyatt Alt <wyaltster@gmail.com>
2016-01-23 03:17:50 -0800

    nrepl

--- src/prosper/service.clj
+++ src/prosper/service.clj
@@ -6,13 +6,19 @@
             [environ.core :refer [env]]
             [prosper.query :refer [request-access-token refresh-access-token]]
             [puppetlabs.trapperkeeper.core :refer [defservice]]
+            [clojure.tools.nrepl.server :refer [start-server stop-server]]
             [overtone.at-at :as atat]))
 
 (defn start-prosper-service
-  [context db release-rate base-rate storage-threads]
-  (future (collection/query-and-store db release-rate base-rate
-                                      storage-threads))
-  context)
+  [db release-rate base-rate storage-threads]
+  (future
+    (collection/query-and-store db release-rate base-rate storage-threads)))
+
+(defn attach-nrepl-server
+  [context {:keys [enabled port] :as nrepl-config}]
+  (if (and nrepl-config (true? enabled))
+    (assoc context :nrepl-server (start-server :port port))
+    context))
 
 (defservice prosper-service
   []
@@ -22,11 +28,12 @@
                {:keys [client-id client-secret
                        username password
                        storage-threads release-rate base-rate]} (env :prosper)
+               nrepl-config (env :nrepl)
                refresh-token #(refresh-access-token
                                 client-id client-secret username password)]
            (log/info "Running migrations")
            (migrate/migrate! db)
            (request-access-token client-id client-secret username password)
            (atat/every (* 10 60 1000) refresh-token cred-refresh-pool)
-           (start-prosper-service context db release-rate base-rate
-                                  storage-threads))))
+           (start-prosper-service db release-rate base-rate storage-threads)
+           (attach-nrepl-server context nrepl-config))))
