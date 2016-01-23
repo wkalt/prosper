b7ec7d313959d2989db25af2acb5cdbaa3d2635c
Wyatt Alt <wyaltster@gmail.com>
2016-01-23 02:44:10 -0800

    remove core.clj

--- src/prosper/core.clj
+++
@@ -1,9 +0,0 @@
-(ns prosper.core
-  (:gen-class)
-  (:require [prosper.collection :as collection]))
-
-(defn -main
-  [& args]
-  (case (first args)
-    "collection" (collection/query-and-store))
-  (System/exit 0))
